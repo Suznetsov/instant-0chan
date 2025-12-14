4fefb44da0d79984c75c3e055c376ce7df2d4786
Suznetsov <jew2shroom@proton.me>
2025-12-14 17:48:37 -0600

    Add files via upload

---
+++ updates.js
@@ -0,0 +1,42 @@
+const node_ip = '127.0.0.1', node_port = '1337'
+, sites = {
+	your_chan_id: 'you_chan_token'
+	// add more sites if you want (sitename: "srvtoken")
+}
+
+const bodyParser = require('body-parser')
+, express = require('express'), app = express()
+, server = require('http').createServer(app)
+, io = require('socket.io')(server)
+app.use(bodyParser.json())
+
+app.post('/qr/:site', (req, res) => {
+	let site = req.params.site, srvtoken
+	if (sites.hasOwnProperty(site))
+		srvtoken = sites[site]
+	else {
+		res.status(403).end()
+		return
+	}
+	let data = req.body
+	if(typeof data.srvtoken !== 'undefined' || data.srvtoken === srvtoken || typeof data.room !== 'undefined') {
+		let rs = data.room.split(':')
+		, room = site+':'+data.room
+		if (!data.board)
+			data.board = rs[0]
+		data.room = rs[1]
+		delete data.srvtoken
+		io.to(room).emit('update', data)
+	}
+	res.status(200).end()
+})
+
+io.on('connection', function (socket) {
+	socket.on('subscribe', function(rooms) {
+		if (!(rooms instanceof Array))
+			rooms = [rooms]
+		rooms.forEach(room => socket.join(room))
+	})
+})
+
+server.listen(node_port, node_ip)
